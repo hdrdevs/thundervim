c9a543c56dc728e7adf1592309e1e2d56bc9b759
Horacio Daniel Ros <horaciodrs@gmail.com>
2023-04-03 14:22:35 -0300

    ubuntu packages

--- init.lua
+++ init.lua
@@ -1,29 +1,23 @@
 require("plugins")
 
-local checklib = nil
+--lfs exists, so use it.
+if package.loaded("lsp-zero") then
+    require("keys")
+    require("settings")
+    require("configtheme")
+    require("thememanager")
+    require("hydra_vimoptions")
 
+    --plugin confis
 
-local status, checklib = pcall(require, "lsp-zero")
-if (status) then
-    --lfs exists, so use it.
-    if checklib ~= nil then
-        require("keys")
-        require("settings")
-        require("configtheme")
-        require("thememanager")
-        require("hydra_vimoptions")
-
-        --plugin confis
-
-        require("configs.cokeline")
-        require("configs.colorizer")
-        require("configs.feline")
-        require("configs.lsp")
-        require("configs.nvimtree")
-        require("configs.project")
-        require("configs.rainbow")
-        require("configs.tablines")
-        require("configs.treesitter")
-        require("configs.whichkey")
-    end
+    require("configs.cokeline")
+    require("configs.colorizer")
+    require("configs.feline")
+    require("configs.lsp")
+    require("configs.nvimtree")
+    require("configs.project")
+    require("configs.rainbow")
+    require("configs.tablines")
+    require("configs.treesitter")
+    require("configs.whichkey")
 end
