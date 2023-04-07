e00e19537149f201ee8f5ad140a000865ed1e701
Horacio Daniel Ros <horaciodrs@gmail.com>
2023-04-06 22:13:37 -0300

    outline plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -235,7 +235,7 @@ return require('packer').startup(function(use)
         end
     })
 
-    use ('simrat39/symbols-outline.nvim', config = function() require("symbols-outline").setup() end)
+    use ({'simrat39/symbols-outline.nvim', config = function() require("symbols-outline").setup() end})
 
 
 end)
